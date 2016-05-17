49fde513abc6eb9b415fed58bd1f80abdea3a2ab
Varun Ramesh <varunramesh@live.com>
2016-05-16 22:23:34 -0700

    Some more customization for server.py

--- server.py
+++ server.py
@@ -1,16 +1,23 @@
+"""
+This file starts a simple TCP server that listens for connections. When a
+connection is established, the server sends a bunch of data through tha
+connection. After sending the data, it closes the connection.
+"""
+
 import SocketServer
 import argparse
 
 parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('port', type=int, help='The port to host the TCP server on.')
+parser.add_argument('--port', default=8000, type=int,
+                    help='The port on which to listen.')
+parser.add_argument('--length', default=100000, type=int,
+                    help='The size of the data to send over the connection.')
 args = parser.parse_args()
 
-DATA_LENGTH = 10000
-DATA = "F" * DATA_LENGTH
+DATA = "F" * args.length
 
 class TCPHandler(SocketServer.BaseRequestHandler):
-    def handle(self):
-        self.request.sendall(DATA)
+    def handle(self): self.request.sendall(DATA)
 
 if __name__ == "__main__":
     server = SocketServer.TCPServer(('localhost', args.port), TCPHandler)
