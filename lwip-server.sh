ae3067dc7ce7f1b00494df683e03c72cf1eda8c5
Varun Ramesh <varunramesh@live.com>
2016-05-26 23:29:22 -0700

    Can now make connection to lwip server.

--- lwip-server.sh
+++ lwip-server.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+sysctl net.ipv4.ip_forward=1
+sysctl net.ipv4.conf.all.proxy_arp=1
+route add default gw 10.11.0.1
+
 ip tuntap add dev tap0 mode tap
 ip link set dev tap0 up
-ip addr add 172.16.0.1/24 dev tap0
-./lwip-tap/lwip-tap -i name=tap0,addr=172.16.0.2,netmask=255.255.255.0,gw=172.16.0.1 -E
+ip addr add 10.11.0.2/16 dev tap0
+./lwip-tap/lwip-tap -i name=tap0,addr=10.11.0.1,netmask=255.255.0.0,gw=10.11.0.2 -E
